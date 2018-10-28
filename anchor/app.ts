7704dad32cb83771571ab298e1a540ad42bb2775
danfergo <danfergo@gmail.com>
2018-10-28 17:45:54 +0000

    changed server creation to use native http server; passing http server to ncmodule constructor;

--- anchor/app.ts
+++ anchor/app.ts
@@ -1,4 +1,6 @@
 import * as express from "express";
+import * as http from 'http';
+
 // import * as urljoin from 'urljoin';
 
 export interface AppOptions {
@@ -10,17 +12,16 @@ export class App {
     static bootstrap(options: AppOptions, module: any) {
 
         const app = express();
+        const server = http.createServer(app);
 
-        let m = new module(app);
+        let m = new module(app, server);
         app.use(m.__router__);
 
 
         const port = options.port || 3000;
-        const server = app.listen(port, () => {
-            console.log(
-                "  App is running at http://localhost:%d in %s mode",
-                port
-            );
+
+        server.listen(port, () => {
+            console.log("  App is running at http://localhost:%d ", port);
             console.log("  Press CTRL-C to stop\n");
         });
     }
